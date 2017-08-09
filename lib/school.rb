d3cba82d0292f451390eda54e10945a4af6b9419
Marlon Merjos <marlonmerjos@gmail.com>
2017-08-09 17:25:28 -0400

    Done.

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,29 @@
-# code here!
+class School
+
+  def initialize(school)
+    @roster = Hash.new
+  end
+
+  def roster
+    @roster
+  end
+
+  def add_student(name, grade)
+    if @roster.keys.include?(grade)
+      @roster[grade] << name
+    else
+      @roster[grade] = [name]
+    end
+  end
+
+  def grade(grade)
+    @roster[grade]
+  end
+
+  def sort #access the roster hash and orders the
+    @roster.each do |grade, students_array|
+      @roster[grade] = students_array.sort
+    end
+  end
+
+end
